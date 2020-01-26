9c34023494eebffb3e84fa8a2cce58d452288bca
leonidas georgas <lgeorgas@gmail.com>
2020-01-26 15:39:22 +1030

    added in sean improvements - slerp for smooth turning

--- Assets/charController.cs
+++ Assets/charController.cs
@@ -29,11 +29,15 @@ public class charController : MonoBehaviour {
 
     void Move()
     {
+        const float turnRateAsMultipleOfSeconds = 10;
+
+        Vector3 currentHeading = transform.forward;
         Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
         Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("Vertical");
-        Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
-
+        Vector3 wantedHeading = Vector3.Normalize(rightMovement + upMovement);
+        Vector3 heading = Vector3.Slerp(currentHeading, wantedHeading, Time.deltaTime * turnRateAsMultipleOfSeconds);
+        
         transform.forward = heading;
         transform.position += rightMovement;
         transform.position += upMovement;
